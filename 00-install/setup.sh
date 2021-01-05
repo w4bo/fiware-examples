355db4dd41da201afc6ec368a0069552a8ede7d7
w4bo <m.francia@unibo.it>
2021-01-05 14:45:35 +0100

    uniforming stop and setup

--- 00-install/setup.sh
+++ 00-install/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-set -e
-sudo docker pull mongo:4.2
-sudo docker pull fiware/orion
-sudo docker network create fiware_default
-sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:4.2 --bind_ip_all --smallfiles
-sudo docker run -d --name fiware-orion -h orion --network=fiware_default -p 1026:1026 fiware/orion -dbhost mongo-db
-sudo docker ps
+git clone https://github.com/FIWARE/tutorials.Getting-Started.git
+cd tutorials.Getting-Started
+sudo docker-compose -p fiware up -d
+cd -
--- 03-json/setup.sh
+++ 03-json/setup.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/FIWARE/tutorials.IoT-Agent-JSON.git
-cd tutorials.IoT-Agent
+cd tutorials.IoT-Agent-JSON
 sudo ./services create; sudo ./services start
